75890b6142a5bc23fa950271a4c72bfdaded3272
Lars van Galen <hallo@webartisist.nlgalen.org>
2023-04-16 15:41:57 +0200

    feat: init package

--- database/factories/ModelFactory.php
+++ database/factories/ModelFactory.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace VendorName\Skeleton\Database\Factories;
+namespace Larsvg\JetstreamUsersRolesPermissionsManagement\Database\Factories;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
 
