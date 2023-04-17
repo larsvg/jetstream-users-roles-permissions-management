3f25c0858a198be1a62ee1d2fc8c95582aac6ca8
Lars van Galen <hallo@webartisist.nlgalen.org>
2023-04-17 17:27:18 +0200

    feat: Edit user action

--- resources/views/users-overview/edit.blade.php
+++ resources/views/users-overview/edit.blade.php
@@ -1,15 +1,46 @@
-
 <x-app-layout>
+    <x-slot name="header">
+        {{ Breadcrumbs::render('profile.show') }}
+    </x-slot>
 
-    <div class="px-8 py-8">
-        <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg p-8">
+    <div>
+        <div class="max-w-7xl mx-auto py-10 sm:px-6 lg:px-8">
 
-            <h1 class="text-2xl font-medium text-gray-900 mb-6">
-                {{ __('users-management::pages/edit-user.title') }}
-            </h1>
+            <x-form-section submit="updateLocale">
+                <x-slot name="title">
+                    {{ __('Todo') }}
+                </x-slot>
+
+                <x-slot name="description">
+                    {{ __('Todo') }}
+                </x-slot>
+
+                <x-slot name="form">
+                    <div class="col-span-6 sm:col-span-4">
+                        <x-label for="current_password" value="{{ __('Todo') }}" />
+                        <x-select
+                            id="courses_listbox"
+                            name="courses[]"
+                            class="dual-listbox"
+                            :options="config('app.avaliable_locales')"
+                            :selected="Auth::user()->locale ?? config('app.locale')"
+                            wire:model.defer="state.language"
+                        />
+
+                        <x-input-error for="current_password" class="mt-2" />
+                    </div>
+
+                </x-slot>
+
+                <x-slot name="actions">
+
+                    <x-button>
+                        {{ __('Save') }}
+                    </x-button>
+                </x-slot>
+            </x-form-section>
 
 
-            //
 
         </div>
     </div>
