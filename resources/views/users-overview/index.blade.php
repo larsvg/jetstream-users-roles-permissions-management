a9a8aaedbae19e6effb63e6969583dca2c40d9c6
Lars van Galen <hallo@webartisist.nlgalen.org>
2023-05-08 13:51:44 +0200

    feat: make page title component

--- resources/views/users-overview/index.blade.php
+++ resources/views/users-overview/index.blade.php
@@ -2,9 +2,10 @@
 <x-app-layout>
 
     <div class="px-0 lg:px-8 py-8 flex flex-col gap-8">
-        <h1 class="text-2xl font-medium text-gray-900">
+
+        <x-page-title>
             {{ __('users-management::pages/users-overview.title') }}
-        </h1>
+        </x-page-title>
 
         @livewire('users-overview')
     </div>
