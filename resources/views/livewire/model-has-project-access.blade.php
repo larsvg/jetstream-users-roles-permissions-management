ba0566b978a1dd120a9cd1811edbf273ade9fa65
Lars van Galen <hallo@webartisist.nlgalen.org>
2024-02-19 16:40:15 +0100

    feat: Toggle model has project access

--- resources/views/livewire/model-has-project-access.blade.php
+++ resources/views/livewire/model-has-project-access.blade.php
@@ -1,3 +1,23 @@
+
 <div>
-    {{-- In work, do what you enjoy. --}}
+
+    @foreach($projects as $project)
+        <div wire:click="toggleAccess('{{ $user->id }}', '{{ $project->id }}')">
+            @php
+                $checked = \App\Models\ModelHasProjectAccess::where('model_id', $user->id)
+                    ->where('project_id', $project->id)
+                    ->exists();
+            @endphp
+
+            <input type="checkbox"  class="rounded border-gray-300 text-indigo-600 shadow-sm focus:ring-indigo-500"
+                   @if($checked) checked @endif
+            >
+
+            <label class="{{ $checked ? '' : 'text-red-700 line-through' }}">
+                {{ __($project->name) }}
+            </label>
+        </div>
+
+    @endforeach
+
 </div>
--- src/Http/Livewire/ModelHasProjectAccess.php
+++ src/Http/Livewire/ModelHasProjectAccess.php
@@ -2,12 +2,43 @@
 
 namespace Larsvg\JetstreamUsersRolesPermissionsManagement\Http\Livewire;
 
+use App\Models\Project;
+use App\Models\User;
 use Livewire\Component;
-
 class ModelHasProjectAccess extends Component
 {
+    public User $user;
+
+    public function mount(User $user): void
+    {
+        $this->user = $user;
+    }
+
     public function render()
     {
-        return view('user-management::livewire.model-has-project-access');
+        $projects = Project::get();
+
+        return view('user-management::livewire.model-has-project-access', [
+            'user'     => $this->user,
+            'projects' => $projects,
+        ]);
     }
+
+    public function toggleAccess(User $user, Project $project)
+    {
+        $model = \App\Models\ModelHasProjectAccess::where('model_id', $user->id)
+            ->where('project_id', $project->id)
+            ->first();
+
+        if ($model) {
+            $model->delete();
+        } else {
+            \App\Models\ModelHasProjectAccess::create([
+                'model_id'    => $user->id,
+                'model_type' => 'App\Models\User',
+                'project_id' => $project->id,
+            ]);
+        }
+    }
+
 }
